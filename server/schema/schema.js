f6e6c0737e202b708e355ca0b8f19494a091af02
Daniel Lee <dlee0528@hotmail.com>
2021-01-23 19:59:09 -0800

    implement graphQLNonNull

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,6 +11,7 @@ const {
     GraphQLID,
     GraphQLInt,
     GraphQLList,
+    GraphQLNonNull,
 } = graphql;
 
 
@@ -82,8 +83,8 @@ const Mutation = new GraphQLObjectType({
         addAuthhor: {
             type: AuthorType,
             args:{
-                name: {type: GraphQLString},
-                age: {type: GraphQLList}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLList)}
             },
             resolve(parent, args){
                 let author  = new Author({
@@ -96,9 +97,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args:{
-                name: {type:  GraphQLString},
-                genre: {type: GraphQLString},
-                authorid: {type: GraphQLID}
+                name: {type: new GrraphQLNonoNull(GraphQLString)},
+                genre: {type: new GraphQLNonNull(GraphQLString)},
+                authorid: {type: new GraphQLNonNull(GraphQLID)}
             },
             resolve(parent, args){
                 let book = new Book({
